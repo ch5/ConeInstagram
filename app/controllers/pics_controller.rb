3b3ed7f09e0752eef094c17aa0f88e99b67a2d7f
ch5 <kholis13@gmail.com>
2017-02-06 09:29:57 +0700

    show on pic by id

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,12 @@
 class PicsController < ApplicationController
-  def index
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @pics = Pic.all
+  end
+
+  def show
+    @pic = Pic.find(params[:id])
   end
 
   def new
@@ -8,12 +14,22 @@ class PicsController < ApplicationController
   end
 
   def create
-    @pic = Pic.new(pic.params)
+    @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "Yess! It was posted!"
+    else
+      render 'new'
+    end
   end
 
   private
 
     def pic_params
       params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+      @pic = Pic.find(params[:id])
     end
 end
